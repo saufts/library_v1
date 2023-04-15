e62d4acdffcd7a80ade960f9d1368672f7678b93
saufts <l.szwaczka90@gmail.com>
2023-04-15 17:28:11 +0200

    Add event listener to button in order to display form to add a book

--- script.js
+++ script.js
@@ -7,6 +7,16 @@ let cardTitle = document.querySelector('.title');
 let cardPages = document.querySelector('.pages');
 let cardStatus = document.querySelector('.status');
 
+const btnAdd = document.querySelector('.btn-add');
+const formAdd = document.querySelector('#form-add-book');
+
+
+
+btnAdd.addEventListener('click', () => {
+  // document.getElementById('#book-add-form').style.display = block;
+  formAdd.style.display = 'block';
+});
+
 function Book(title, author, pages, status) {
   this.title = title;
   this.author = author;
